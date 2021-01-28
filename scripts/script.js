f4c07c235d27a8f00f43982aa9103a9f98fb67fc
makersmarx <bbiehn1@gmail.com>
2021-01-28 07:04:41 -0600

    practice with local storage w JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
+const localStore = () => {
+  const getName = document.getElementById('username');
+  const password = document.getElementById('password');
 
+  if (getName.value.length === 0) {
+    alert('Please try again');
+  } else if (password.value.length === 0) {
+    alert('Please try again');
+  } else if (getName.value.length === 0 && password.value.length == 0) {
+    alert('Please fill in email and password');
+  } else {
+    localStorage.setItem('name', getName.value);
+    localStorage.setItem('password', password.value);
+    alert('Your account has been created');
+  }
+};
+
+//checking
+const check = () => {
+  const storedName = localStorage.getItem('getName');
+  const storedPassword = localStorage.getItem('password');
+
+  const userName = document.getElementById('username');
+  const userPassword = document.getElementById('password');
+
+  if (userName.value === storedName && userPassword.value === storedPassword) {
+    alert('You are logged in.');
+  } else {
+    alert('Error on login');
+  }
+};
