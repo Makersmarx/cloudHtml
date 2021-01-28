32c924a2b7c64f3c0fbff626f55903e088cd2ecb
makersmarx <bbiehn1@gmail.com>
2021-01-28 09:44:04 -0600

    change var names for readability

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,27 @@
 const storeUser = () => {
-  const storeName = document.getElementById('username');
-  const password = document.getElementById('password');
+  const storeName = document.getElementById('userName');
+  const password = document.getElementById('userPassword');
 
   if (storeName.value.length === 0 || password.value.length === 0) {
     alert('Please try again');
   } else {
-    localStorage.setItem('username', storeName.value);
-    localStorage.setItem('password', password.value);
+    localStorage.setItem('userName', storeName.value);
+    localStorage.setItem('userPassword', password.value);
     alert('Your account has been created');
-    window.location.href = 'index.html';
+    window.location.href = '../index.html';
   }
 };
 
 //check local storage for user
 const checkUser = () => {
-  const storedName = localStorage.getItem('username');
-  const storedPassword = localStorage.getItem('password');
+  const checkName = localStorage.getItem('username');
+  const checkPassword = localStorage.getItem('password');
 
   const userName = document.getElementById('userName');
   const userPassword = document.getElementById('userPassword');
 
-  if (userName.value === storedName && userPassword.value === storedPassword) {
-    window.location.href = '../index.html';
+  if (userName.value === checkName && userPassword.value === checkPassword) {
+    window.location.href = '../indexedDB.html';
   } else {
     alert('Error on login');
   }
